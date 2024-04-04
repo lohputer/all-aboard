836e0062febe032e718b46f6b52aae5625f8bc5d
Lyndon Loh <leynalyndon@gmail.com>
2024-04-05 00:25:20 +0800

    b

--- backend/base/api/consumers.py
+++ backend/base/api/consumers.py
@@ -4,24 +4,21 @@ from channels.generic.websocket import WebsocketConsumer
 
 class TextRoomConsumer(WebsocketConsumer):
     def connect(self):
-
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
         self.accept()
+
     def disconnect(self, close_code):
-        # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
     def receive(self, text_data):
-        # Receive message from WebSocket
         text_data_json = json.loads(text_data)
         text = text_data_json['text']
         sender = text_data_json['sender']
@@ -36,10 +33,8 @@ class TextRoomConsumer(WebsocketConsumer):
         )
 
     def chat_message(self, event):
-        # Receive message from room group
         text = event['message']
         sender = event['sender']
-        # Send message to WebSocket
         self.send(text_data=json.dumps({
             'text': text,
             'sender': sender
--- backend/base/api/routing.py
+++ backend/base/api/routing.py
@@ -1,16 +1,12 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
-# import app.routing
 from django.urls import re_path
-from consumers import TextRoomConsumer
-
+from .consumers import TextRoomConsumer
 websocket_urlpatterns = [
     re_path(r'^ws/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
 ]
-# the websocket will open at 127.0.0.1:8000/ws/<room_name>
 application = ProtocolTypeRouter({
     'websocket':
         URLRouter(
             websocket_urlpatterns
-        )
-    ,
+        ),
 })
